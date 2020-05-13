54f7815e3ea891b65277a1be6393701551b818d3
Leha228 <ryabin.01@mail.ru>
2020-05-13 12:02:16 +0500

    Изменил форму регистрации

--- resources/views/reg.blade.php
+++ resources/views/reg.blade.php
@@ -3,23 +3,28 @@
 <form class="mt-5" action="{{ route('reg-submit') }}" method="post">
 @csrf
   <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
+    <label for="exampleInputEmail1">Имя</label>
+    <input type="name" class="form-control" id="exampleInputEmail1">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Фамилия</label>
+    <input type="name" class="form-control" id="exampleInputEmail1">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
+    <label for="exampleInputPassword1">Пароль</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Confirm Password</label>
+    <label for="exampleInputPassword1">Повторить пароль</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
 </form>
 @endsection
