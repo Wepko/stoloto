0971a047e6c83adb22427473c9f889ad9efcd567
Leha228 <ryabin.01@mail.ru>
2020-05-13 12:39:18 +0500

    Изменил форму авторизациии

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,12 +3,12 @@
 <form class="mt-5" action="{{ route('login-submit') }}" method="post">
 @csrf
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
+    <label for="exampleInputEmail1">Email</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
+    <label for="exampleInputPassword1">Пароль</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
