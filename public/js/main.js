fabcd5e8d7da0e8ea29f41942eb179854e6c6884
Wepko <Wepko.pro@mail.ru>
2020-05-15 15:19:24 +0500

    edit checkbox js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
+let count = 0;
 function addTicket() {
-    let parent = document.getElementById('test');
-    let elem1 = parent.querySelector('.card');
+    count += 1
+    const parent = document.getElementById('test')
+    const elem = parent.querySelector('.card')
 
-    let clone1 = elem1.cloneNode(true);
-    parent.appendChild(clone1);
+    const clone = elem.cloneNode(true)
+
+    const arrTagInput = [...clone.querySelectorAll('input')]
+
+    for (let tagInput of arrTagInput) {
+          tagInput.name = `ticket${count + 1}_fieldOne`
+        console.log(tagInput)
+    }
+
+    parent.appendChild(clone)
 
 }
 
