35f5038d52046d8320338a08f13e3d90eb3e5def
Wepko <Wepko.pro@mail.ru>
2020-05-27 16:04:50 +0500

    add

--- resources/views/games/one-game.blade.php
+++ resources/views/games/one-game.blade.php
@@ -14,31 +14,9 @@
         </div>
     </div>
     <form action="{{ route('AddTicketValueOne') }}" method='post'>
-<<<<<<< Updated upstream
         @csrf   
         <input type="number" name="valid" class="form-control valid d-none" id="validTicketNumber">
-=======
-        @csrf
-        <input type="number" name="valid" class="form-control valid" id="validTicketNumber" value="1" style="display:none">
-            <div class="blocks-ticket " id="test">
-                <div class="block-ticket card">
-                    <div class="help-information">
-                        <p>Отметьте не менее 4 чисел в каждом поле.</p>
-                        <a href="#" class="btn btn-dark" onclick="addTicket()" id="addTicket">Добавить билет</a>
-                    </div>
-    
-                    <div class="zone-headers">
-                        <div class="zone-header">Поле 1 <p></p></div>
-                        <div class="zone-header">Поле 2 <p></p></div>
-                    </div>
-                
-                    <div class="zone-worker" id="zone-worker">
-                        <div class="zone-one" >
-                            
-                        </div>
-                        <div class="zone-two">
- 
->>>>>>> Stashed changes
+
         <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
