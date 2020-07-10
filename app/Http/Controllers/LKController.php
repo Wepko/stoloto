4dadb36366b46cc6817337ccd884902667f96032
Leha228 <ryabin.01@mail.ru>
2020-07-10 19:23:21 +0500

    Update

--- app/Http/Controllers/LKController.php
+++ app/Http/Controllers/LKController.php
@@ -17,15 +17,16 @@ class LKController extends Controller
 
     public function index() {
 
-        $billPayments = new \Qiwi\Api\BillPayments('eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjdkN2MyMmI1LWJjMTctNDU1Zi04NTdjLTExYjA1OTI1YTgyZCIsInVzZXJfaWQiOiI3OTYyNjg1MzQ1OCIsInNlY3JldCI6IjRjN2UzZjkxMjg5YTEwYTg3NTRiYjY0MDU1NTMyMGU1OTQ2NjBjZWE2ZTdhZjlkYTQyMWQ2NGU3ZmVjOGM0ODUifX0=');
+        $billPayments = new \Qiwi\Api\BillPayments('eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjdkN2MyMmI1LWJjMTctNDU1Zi04NTdjLTExYjA1OTI1YTgyZCIsInVzZXJfaWQiOiI3OTYyNjg1MzQ1OCIsInNlY3JldCI6ImM2ZmVmNDBmYWM1ZTU1MDZkYmQyODk0OTJkODIzNmM2NTZlM2I5YjU5MTgzNDA2YmIzYzQ0ZmEwZTI2NWQ3Y2QifX0=');
         
         if (DB::table('refill')->where('user_id', Auth::user()->getId())->get()) {
+            
             $refill = DB::table('refill')->where('user_id', Auth::user()->getId())->get();
+
             foreach ($refill as $ref) {
                 $billId = $ref->id;
                 /** @var \Qiwi\Api\BillPayments $billPayments */
                 $response = $billPayments->getBillInfo($billId);
-            
                 if ($response["status"]["value"] == "PAID") {
                     $model = User::where('id', '=', Auth::user()->getId())->first();
                     
@@ -48,7 +49,7 @@ class LKController extends Controller
 
     public function refill(Request $request) {
 
-        $billPayments = new \Qiwi\Api\BillPayments('eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjdkN2MyMmI1LWJjMTctNDU1Zi04NTdjLTExYjA1OTI1YTgyZCIsInVzZXJfaWQiOiI3OTYyNjg1MzQ1OCIsInNlY3JldCI6IjRjN2UzZjkxMjg5YTEwYTg3NTRiYjY0MDU1NTMyMGU1OTQ2NjBjZWE2ZTdhZjlkYTQyMWQ2NGU3ZmVjOGM0ODUifX0=');
+        $billPayments = new \Qiwi\Api\BillPayments('eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjdkN2MyMmI1LWJjMTctNDU1Zi04NTdjLTExYjA1OTI1YTgyZCIsInVzZXJfaWQiOiI3OTYyNjg1MzQ1OCIsInNlY3JldCI6ImM2ZmVmNDBmYWM1ZTU1MDZkYmQyODk0OTJkODIzNmM2NTZlM2I5YjU5MTgzNDA2YmIzYzQ0ZmEwZTI2NWQ3Y2QifX0=');
         $count = DB::table('refill')->max('id');
 
         RefillModels::insert(array(
@@ -56,7 +57,7 @@ class LKController extends Controller
             'price' => $request->input('price')
         ));
 
-        $publicKey = '2S7mpWSvB93qSAr7uYNu2Vvnd2pTVzxEviw6chKKbG9xyy9pcxcvrmne6c6m7cUabcbN8Gnkjk77SEeN2YVBjf2bWuqw15YpX498gZpHhZZ32mxbfN9SmUVRUnNZGG4ucyHkByAyivprASsyt8xT7jw6BT35LDVBPEEYu4RLRFrfMTsmRXNnaHnVXZ412vsjmofXKncSw5jR4sd5VgeWQbduXF5U83HEW2';
+        $publicKey = '2S7mpWSvB93qSAr7uYNu2Vvnd2pTVzxEviw6chKKbG9xyy9pcxcvrmne6c6m7cUabcbN8Gnkjk77SEeN2YVBjf2bWuqw15YpX498gZpHhZZ32mxbfN9SmUVRUnNZGG4ucyHkByAyivprASsyt8xT7jw6BT35LDVBPEEYu4RLRFrfMS9JL6WcUMiduJMUAAoWip5q91t5aQGRX9J24EDrx8vhp9H25eS5a4';
         
         $params = [
             'publicKey' => $publicKey,
