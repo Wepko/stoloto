86efa2f4d24c5933f6855b2df95e0b2132fe0c33
Leha228 <ryabin.01@mail.ru>
2020-08-10 10:09:14 +0500

    Update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/games/speed-game/win'
+        'https://www.win-1.ru/games/speed-game/win'
     ];
 }
